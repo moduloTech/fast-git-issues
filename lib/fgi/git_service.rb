f43ab33c11f3bf9211f362b48623587dbc52ac1d
Matthieu Gourvénec <matthieu.gourvenec@gmail.com>
2017-09-19 18:04:11 +0200

    fixed git remote names

--- lib/fgi/git_service.rb
+++ lib/fgi/git_service.rb
@@ -74,7 +74,7 @@ module Fgi
         end
         %x(git checkout #{CONFIG[:default_branch]}) # Be sure to be on the default branch.
         from = %x(git branch | grep '*').gsub('* ', '').chomp
-        git_remote = %x(git remote)
+        git_remote = %x(git remote).chomp
         %x(git pull #{git_remote} HEAD) # Be sure to get the remote changes locally.
         %x(git checkout -b #{branch_name}) # Create the new branch.
         to = %x(git branch | grep '*').gsub('* ', '').chomp
